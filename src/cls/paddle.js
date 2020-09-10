c177d1a0ae05bd891eb7ffbdd7446df88c820da5
GalaxyGamingBoy <53054461+GalaxyGamingBoy@users.noreply.github.com>
2020-09-10 15:36:05 +0300

    Added User Input

--- src/cls/paddle.js
+++ src/cls/paddle.js
@@ -2,7 +2,10 @@ export default class Paddle {
   constructor(gameWidth, gameHeight) {
     this.width = 150;
     this.height = 30;
+    this.gameWidth = gameWidth;
     this.color = "#FFA033";
+    this.maxSpeed = 10;
+    this.speed = 0;
     this.position = {
       x: gameWidth / 2 - this.width / 2,
       y: gameHeight - this.height - 10
@@ -10,9 +13,22 @@ export default class Paddle {
   }
   update(deltaTime) {
     if (!deltaTime) return;
+    if (this.position.x < 0) this.position.x = 0;
+    if (this.position.x + this.width > this.gameWidth)
+      this.position.x = this.gameWidth - this.width;
+    this.position.x += this.speed;
   }
   draw(ctx) {
     ctx.fillStyle = this.color;
     ctx.fillRect(this.position.x, this.position.y, this.width, this.height);
+  }
+  moveLeft() {
+    this.speed = -this.maxSpeed;
+  }
+  moveRight() {
+    this.speed = this.maxSpeed;
+  }
+  stop() {
+    this.speed = 0;
   }
 }
--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 //index.js
 import Paddle from "/src/cls/paddle";
+import InputHandler from "/src/cls/input";
 
 let canvas = document.getElementById("screenCanvas");
 let ctx = canvas.getContext("2d");
@@ -7,7 +8,9 @@ const GAME_WIDTH = 800;
 const GAME_HEIGHT = 600;
 
 let paddle = new Paddle(GAME_WIDTH, GAME_HEIGHT);
+
 paddle.draw(ctx);
+new InputHandler(paddle);
 
 let lastTime = 0;
 function gameLoop(timestamp) {
